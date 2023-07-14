de9752848f77660d220acaf5682c39559dfb1214
JALG27 <41845529+JALG27@users.noreply.github.com>
2023-07-14 13:10:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ import authRoutes from "./routes/authRoute.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+/*import path from "path"*/
+/*import { fileURLToPath } from "url";*/
 
 //configure env
 dotenv.config();
@@ -16,9 +16,9 @@ dotenv.config();
 //database config
 connectDB();
 
-//esmodulefix
+/*//esmodulefix
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = path.dirname(__filename);*/
 
 //rest object
 const app = express();
@@ -32,12 +32,17 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
-app.use(express.static(path.join(__dirname, './client/public')))
+/*app.use(express.static(path.join(__dirname, './client/public')))*/
 
 //rest api
-app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/public'));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Bienvenido a TecnoExpress</h1>");
 });
+
+/*app.use('*',function(req,res){
+  res.sendFile(path.join(__dirname,'./client/public'));
+});*/
 
 //PORT
 const PORT = process.env.PORT || 8080;
